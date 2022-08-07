ada8cc7ce32d4f0d86a8e6df3d46937428b9445f
Freya Ariel <hifreyaariel@icloud.com>
2022-08-07 02:49:22 +0200

    Improve work-in-progress build script slightly

--- Scripts/Build.sh
+++ Scripts/Build.sh
@@ -1,11 +1,29 @@
 #!/bin/sh
 
+# TODO: either recover hints file from old laptop or recreate it
+
+PERL_VERSION=5.30.3
+PERL_CROSS_VERSION=1.3.7
+
+pushd `mktemp -d`
+
+curl -L -O http://www.cpan.org/src/5.0/perl-$PERL_VERSION.tar.gz
+curl -L -O https://github.com/arsv/perl-cross/releases/download/$PERL_CROSS_VERSION/perl-cross-$PERL_CROSS_VERSION.tar.gz
+
+tar -zxf perl-$PERL_VERSION.tar.gz
+cd perl-$PERL_VERSION
+tar --strip-components=1 -zxf ../perl-cross-$PERL_CROSS_VERSION.tar.gz
+
 # TODO: patch perl-cross: replace `#!/bin/sh` with `#!/bin/bash`
 
 # brew install gnu-sed binutils
 
+# TODO: don’t assume cowsay is installed
+
 # make sure `sed` is GNU sed
-export PATH=/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin:/usr/local/Cellar/binutils/2.37/bin:$PATH
+# FIXME: fails when multiple versions are installed
+# TODO: use something smarter than `*`
+export PATH=`echo /usr/local/Cellar/gnu-sed/*/libexec/gnubin`:`echo /usr/local/Cellar/binutils/*/bin`:$PATH
 
 # TODO: patch perl:
 
@@ -42,8 +60,10 @@ export PATH=/usr/local/Cellar/gnu-sed/4.8/libexec/gnubin:/usr/local/Cellar/binut
 
 # toke.c (XSUB.h?): replace `PerlProc_execv(ipath, EXEC_ARGV_CAST(newargv));` with `//(ipath, EXEC_ARGV_CAST(newargv));` or smth
 
-#mkdir -p include
-#cp *.h include
+mkdir -p include
+cp *.h include
+
+cowsay "Building for MacOSX (aarch64-apple-mac)"
 
 HOSTCFLAGS="-Wno-compound-token-split-by-macro -Wno-incompatible-pointer-types-discards-qualifiers -Wno-string-plus-int" \
 CFLAGS="-arch arm64 -arch x86_64 -fno-common -DPERL_DARWIN -mmacosx-version-min=11.0 -fno-strict-aliasing -pipe -fstack-protector-strong -I/Applications/Xcode.app/Contents/Developer/Platforms/MacOSX.platform/Developer/SDKs/MacOSX.sdk/usr/include -DPERL_USE_SAFE_PUTENV -std=c89 -O3 -Wall -Werror=pointer-arith -Wextra -Wc++-compat -Wwrite-strings -Werror=declaration-after-statement -Wno-compound-token-split-by-macro -Wno-unused-function -Wno-unused-parameter -Wno-unused-local-typedef -Wno-unused-variable -Wno-incompatible-pointer-types-discards-qualifiers -Wno-sign-compare -Wno-string-plus-int" \
@@ -61,19 +81,23 @@ CFLAGS="-arch arm64 -arch x86_64 -fno-common -DPERL_DARWIN -mmacosx-version-min=
 
 make libperl
 
-#HOSTCFLAGS="-Wno-compound-token-split-by-macro -Wno-incompatible-pointer-types-discards-qualifiers -Wno-string-plus-int" \
-#CFLAGS="-arch armv7k -arch arm64_32 -fno-common -DPERL_DARWIN -mwatchos-version-min=7.0 -fno-strict-aliasing -pipe -fstack-protector-strong -I/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include -DPERL_USE_SAFE_PUTENV -std=c89 -O3 -Wall -Werror=pointer-arith -Wextra -Wc++-compat -Wwrite-strings -Werror=declaration-after-statement -Wno-compound-token-split-by-macro -Wno-unused-function -Wno-unused-parameter -Wno-unused-local-typedef -Wno-unused-variable -Wno-incompatible-pointer-types-discards-qualifiers -Wno-sign-compare -Wno-string-plus-int" \
-#./configure --target=aarch64-apple-ios \
-#--with-cc=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc \
-#--with-ar=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ar \
-#--with-nm=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/nm \
-#--with-ranlib=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib \
-#--with-readelf=/usr/local/Cellar/x86_64-elf-binutils/2.38/x86_64-elf/bin/readelf \
-#--with-objdump=/usr/local/Cellar/x86_64-elf-binutils/2.38/x86_64-elf/bin/objdump \
-#--prefix=/dev/null \
-#--sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk \
-#--no-dynaloader \
-#--disable-mod=B,Devel/Peek,Errno
+cowsay "Building for WatchOS (aarch64-apple-ios)"
+
+HOSTCFLAGS="-Wno-compound-token-split-by-macro -Wno-incompatible-pointer-types-discards-qualifiers -Wno-string-plus-int" \
+CFLAGS="-arch armv7k -arch arm64_32 -fno-common -DPERL_DARWIN -mwatchos-version-min=7.0 -fno-strict-aliasing -pipe -fstack-protector-strong -I/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk/usr/include -DPERL_USE_SAFE_PUTENV -std=c89 -O3 -Wall -Werror=pointer-arith -Wextra -Wc++-compat -Wwrite-strings -Werror=declaration-after-statement -Wno-compound-token-split-by-macro -Wno-unused-function -Wno-unused-parameter -Wno-unused-local-typedef -Wno-unused-variable -Wno-incompatible-pointer-types-discards-qualifiers -Wno-sign-compare -Wno-string-plus-int" \
+./configure --target=aarch64-apple-ios \
+--with-cc=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/cc \
+--with-ar=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ar \
+--with-nm=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/nm \
+--with-ranlib=/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/ranlib \
+--with-readelf=/usr/local/Cellar/x86_64-elf-binutils/2.38/x86_64-elf/bin/readelf \
+--with-objdump=/usr/local/Cellar/x86_64-elf-binutils/2.38/x86_64-elf/bin/objdump \
+--prefix=/dev/null \
+--sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/WatchOS.platform/Developer/SDKs/WatchOS.sdk \
+--no-dynaloader \
+--disable-mod=B,Devel/Peek,Errno
+
+cowsay "Building for AppleTVSimulator (aarch64-apple-ios)"
 
 HOSTCFLAGS="-Wno-compound-token-split-by-macro -Wno-incompatible-pointer-types-discards-qualifiers -Wno-string-plus-int" \
 CFLAGS="-fno-common -DPERL_DARWIN -mtvos-version-min=9.0 -fno-strict-aliasing -pipe -fstack-protector-strong -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk/usr/include -DPERL_USE_SAFE_PUTENV -std=c89 -O3 -Wall -Werror=pointer-arith -Wextra -Wc++-compat -Wwrite-strings -Werror=declaration-after-statement -Wno-compound-token-split-by-macro -Wno-unused-function -Wno-unused-parameter -Wno-unused-local-typedef -Wno-unused-variable -Wno-incompatible-pointer-types-discards-qualifiers -Wno-sign-compare -Wno-string-plus-int" \
@@ -88,6 +112,8 @@ CFLAGS="-fno-common -DPERL_DARWIN -mtvos-version-min=9.0 -fno-strict-aliasing -p
 --sysroot=/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVSimulator.platform/Developer/SDKs/AppleTVSimulator.sdk \
 --no-dynaloader \
 --disable-mod=B,Devel/Peek,Errno
+
+cowsay "Building for AppleTVOS (aarch64-apple-ios)"
 
 HOSTCFLAGS="-Wno-compound-token-split-by-macro -Wno-incompatible-pointer-types-discards-qualifiers -Wno-string-plus-int" \
 CFLAGS="-arch arm64 -fno-common -DPERL_DARWIN -mtvos-version-min=9.0 -fno-strict-aliasing -pipe -fstack-protector-strong -I/Applications/Xcode.app/Contents/Developer/Platforms/AppleTVOS.platform/Developer/SDKs/AppleTVOS.sdk/usr/include -DPERL_USE_SAFE_PUTENV -std=c89 -O3 -Wall -Werror=pointer-arith -Wextra -Wc++-compat -Wwrite-strings -Werror=declaration-after-statement -Wno-compound-token-split-by-macro -Wno-unused-function -Wno-unused-parameter -Wno-unused-local-typedef -Wno-unused-variable -Wno-incompatible-pointer-types-discards-qualifiers -Wno-sign-compare -Wno-string-plus-int" \
@@ -122,3 +148,5 @@ xcodebuild -create-xcframework \
 
 # TODO: add some kind of future Swift Package Manager support for toggling which Perl modules to statically include?
 # TODO: consider adding support for Mac Catalyst
+# TODO: consider PR’ing all the patches to perl-cross
+
